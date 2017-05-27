af022e871938cfc9e0423dc218936201513f8edf
Panagiotis Papadimitropoulos <ppapadim12@gmail.com>
2017-05-27 20:13:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
+def oxford_comma(my_arr)
+
+  case my_arr.size
+  when 1 then my_arr.join
+  when 2 then my_arr.join(" and ")
+  else
+    my_arr[-1] = "and #{my_arr[-1]}"
+    my_arr.join(", ")
+  end
 
 end
